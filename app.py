b1f00ce7bab7729b23cba72bb07c1c26ef422e25
junemel <meljune610@gmail.com>
2023-05-25 20:26:25 +0800

    Tumor Detection Web App

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  #model=tf.keras.models.load_model('.h5')
+  model=tf.keras.models.load_model('TumorDetection.h5')
   return model
 model=load_model()
 st.write("""
@@ -15,7 +15,7 @@ import cv2
 from PIL import Image,ImageOps
 import numpy as np
 def import_and_predict(image_data,model):
-    size=(120,120)
+    size=(128,128)
     image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
     img=np.asarray(image)
     img_reshape=img[np.newaxis,...]
@@ -27,6 +27,7 @@ else:
     image=Image.open(file)
     st.image(image,use_column_width=True)
     prediction=import_and_predict(image,model)
-    #class_names=['']
+    class_names=['Glioma','Meningioma',
+                 'No Tumor','Pituitary']
     string="PREDICTION : "+class_names[np.argmax(prediction)]
     st.success(string)
